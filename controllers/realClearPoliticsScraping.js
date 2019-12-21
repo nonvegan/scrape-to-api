5957eb1f5c4f82c82e163e6c9848ec02c4f5d0f0
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2019-12-21 17:35:19 +0000

    removed extra dependencies, added proper console logs

--- controllers/realClearPoliticsScraping.js
+++ controllers/realClearPoliticsScraping.js
@@ -8,11 +8,12 @@ async function fetchData(siteUrl) {
 }
 
 async function trumpApprovalRead(req, res) {
+    const siteUrl = "https://www.realclearpolitics.com/epolls/other/president_trump_job_approval-6179.html";
     const date = new Date();
-    const $ = await fetchData("https://www.realclearpolitics.com/epolls/other/president_trump_job_approval-6179.html");
+    const $ = await fetchData(siteUrl);
     const approve = $('.polls > #container > .alpha-container  > .alpha > .chart_wrapper > .chart_header > .chart_legend > tbody > tr:nth-child(1)  > .candidate > .value').text();
     const disapprove = $('.polls > #container > .alpha-container  > .alpha > .chart_wrapper > .chart_header > .chart_legend > tbody > tr:nth-child(2) > .candidate > .value').text();
-    console.log("teste")
+    console.log("Fetching trump approval data from " + siteUrl)
     res.send({
         date: date,
         approve: approve,
@@ -21,16 +22,17 @@ async function trumpApprovalRead(req, res) {
 }
 
 async function directionOfCountryRead(req, res) {
+    const siteUrl = "https://www.realclearpolitics.com/epolls/other/direction_of_country-902.html";
     const date = new Date();
-    const $ = await fetchData('https://www.realclearpolitics.com/epolls/other/direction_of_country-902.html');
+    const $ = await fetchData(siteUrl);
     const right = $('.polls > #container > .alpha-container  > .alpha > .chart_wrapper > .chart_header > .chart_legend > tbody > tr  > .candidate:nth-child(1) > .value').text();
     const wrong = $('.polls > #container > .alpha-container  > .alpha > .chart_wrapper > .chart_header > .chart_legend > tbody > tr > .candidate:nth-child(2) >.value').text();
-    console.log("teste")
-    res.send(['directionOfCountry', {
+    console.log("Fetching direction of country data from " + siteUrl)
+    res.send({
         date: date,
         right: right,
         wrong: wrong
-    }])
+    })
 }
 
 
@@ -38,16 +40,3 @@ module.exports = {
     trumpApprovalRead: trumpApprovalRead,
     directionOfCountryRead: directionOfCountryRead
 }
-
-
-
-/*  var jsonContent = JSON.stringify(data);
-
-   var array =
-       fs.wr("output.json", jsonContent, 'utf8', function(err) {
-           if (err) {
-               console.log("An error occured while writing JSON Object to File.");
-               return console.log(err);
-           }
-           console.log("JSON file has been saved.");
-       }); */

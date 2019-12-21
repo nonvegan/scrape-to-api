d914b020ad5551e7b24e5464c0372f66a86b9991
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2019-12-21 20:23:15 +0000

    added simple anchors to root page

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const realClearPoliticsScrapingController = require('../controllers/realClearPoliticsScraping.js')
-
+router.get('/', function(req, res) {
+    res.send("<a href=\"/trump\">trump</a><br><a href=\"/country\">country</a>")
+});
 router.get('/trump', realClearPoliticsScrapingController.trumpApprovalRead);
 router.get('/country', realClearPoliticsScrapingController.directionOfCountryRead);
 

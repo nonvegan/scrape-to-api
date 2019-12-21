5bcb624eb3a8cc06f001d4e79757afd3fc329850
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2019-12-21 21:11:49 +0000

    work on root page

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const realClearPoliticsScrapingController = require('../controllers/realClearPoliticsScraping.js')
 router.get('/', function(req, res) {
-    res.send("<ul><li href=\"/trump\">Trump Approval Rating</li><li href=\"/country\" >Direction of Country</li></ul>");
+    res.send("<ul><li><a href=\"/trump\">Trump Approval Rating</a></li><li><a  href=\"/country\">Direction of Country</a></li></ul>");
 });
 router.get('/trump', realClearPoliticsScrapingController.trumpApprovalRead);
 router.get('/country', realClearPoliticsScrapingController.directionOfCountryRead);
